81f55f6b0341af7be02b92453cab266f8ce39f67
rajahmad <mdrajahmad686@gmail.com>
2026-04-13 14:16:52 -0400

    willko admin

--- config/db.php
+++ config/db.php
@@ -1,9 +1,13 @@
 <?php
 // File: admin_panel/config/db.php
+
+
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "wilkoservices";
+$username = "u479142652_willko_dbuser";
+$password = "Rajbhai@101";
+$dbname = "u479142652_willko_dbname";
+
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
